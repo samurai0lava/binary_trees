4d4f018e4dd8a4d96c038d91c4a329375a4199af
Manalhub <manalrahali24@gmail.com>
2023-08-30 17:40:12 -0700

    fix warning

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,11 +25,14 @@ size_t _binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	int left_height = _binary_tree_height(tree->left);
-	int right_height = _binary_tree_height(tree->right);
+	left_height = _binary_tree_height(tree->left);
+	right_height = _binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,14 +27,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	binary_tree_t *l, *r;
+	size_t left_height;
+	size_t right_height;
 
 	if (tree == NULL)
 		return (0);
 
 	l = tree->left;
 	r = tree->right;
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 	{
